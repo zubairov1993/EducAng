57893230546745e395eec4a685b99f829f699826
vadim <vadim.zubairov1993@gmail.com>
2021-01-13 19:42:59 +0500

    'finish-directory'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 export interface Post {
   title: string
@@ -11,29 +11,6 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {title: 'хочу выучить ангуляр компоненты', text: 'еще учу', id: 1},
-    {title: 'следующи блок', text: 'про компоненты', id: 2}
-  ]
+export class AppComponent {
 
-  updatePosts(post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    console.log('id to remove: ', id);
-    this.posts = this.posts.filter(p => p.id !== id)
-  }
-
-  ngOnInit(): void {
-    // setTimeout( () => {
-    //   console.log('setTimeout');
-    //   this.posts[0] = {
-    //     title: 'changed',
-    //     text: 'changed3',
-    //     id: 33
-    //   }
-    // },5000)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
   ],
   imports: [
     BrowserModule,
