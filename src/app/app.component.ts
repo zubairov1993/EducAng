9531b46b818e49c561233d3fd2ca6a0f94d5b16b
vadim <vadim.zubairov1993@gmail.com>
2021-01-13 19:44:58 +0500

    'start-directory'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
