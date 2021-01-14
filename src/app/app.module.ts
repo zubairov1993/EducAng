4182937206cc43f629f3bb5c7553f60c76210a41
vadim <vadim.zubairov1993@gmail.com>
2021-01-14 19:53:41 +0500

    'start-services'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MultByPipe } from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe,
-    ExMarksPipe,
-    FilterPipe
+
   ],
   imports: [
     BrowserModule,
