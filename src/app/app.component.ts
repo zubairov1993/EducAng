5486a24f07d51ad69c6b55731aeb8e98ec8598d4
vadim <vadim.zubairov1993@gmail.com>
2021-01-18 12:45:50 +0500

    'start-routing'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo, TodosService } from './todos.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit  {
+export class AppComponent  {
 
-  todos: Todo[] = []
-  loading = false
-  todoTitle = ''
-  error = ''
-
-  constructor(private todosService: TodosService) {}
-
-  ngOnInit() {
-    this.fetchTodos()
-  }
-
-  addTodo() {
-    if(!this.todoTitle.trim()) {
-      return
-    }
-
-    this.todosService.addTodo({
-      title: this.todoTitle,
-      completed: false
-    }).subscribe((todo) => {
-      this.todos.push(todo)
-      this.todoTitle = ''
-    })
-  }
-
-  fetchTodos() {
-    this.loading = true
-    this.todosService.fetchTodos()
-    .subscribe((todos: Todo[]) => {
-      this.todos = todos
-      this.loading = false
-    }, error => {
-      this.error = error.message
-    })
-  }
-
-  removeTodo(id: number) {
-    this.todosService.removeTodo(id)
-    .subscribe(() => {
-      this.todos = this.todos.filter(t => t.id !== id)
-    })
-  }
-
-  completeTodo(id: number) {
-    this.todosService.completeTodo(id).subscribe(todo => {
-      this.todos.find(t => t.id === todo.id).completed = true
-    })
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { NgModule, Provider } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
-import { AuthIntercepter } from './auth.intercepter';
 
 
-const INTERCEPTOR_PROVIDER: Provider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthIntercepter,
-  multi: true
-}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,10 +15,8 @@ const INTERCEPTOR_PROVIDER: Provider = {
     BrowserModule,
     FormsModule,
     CommonModule,
-    HttpClientModule
   ],
   providers: [
-    INTERCEPTOR_PROVIDER
   ],
   bootstrap: [AppComponent]
 })
