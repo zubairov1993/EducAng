43f6bad7db9bdd297861376e77a46be6b6e4ec91
vadim <vadim.zubairov1993@gmail.com>
2021-01-19 12:55:00 +0500

    'start-additional'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,17 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AppRoutingModule} from './app-routing.module'
-import { SharedModule } from './shared/shared.module'
+import {FormsModule} from '@angular/forms';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
