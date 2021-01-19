0902e7c5474afc59a8281c8e0ab4ef8271a0d67d
vadim <vadim.zubairov1993@gmail.com>
2021-01-19 12:49:44 +0500

    'finish-work-module'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
 
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)}
+]
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: 'about', component: AboutPageComponent, children: [
-      {path: 'extra', component: AboutExtraPageComponent}
-    ]},
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
